1e2caac6d65e42f1d0f2bbd10709d35cd244f1c6
21120534 <21120534@student.hcmus.edu.vn>
2025-03-20 16:10:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mid-term-data_visualization" 
---
+++ backend/src/main/java/com/example/visualize/VisualizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.visualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizeApplication.class, args);
+	}
+
+}
